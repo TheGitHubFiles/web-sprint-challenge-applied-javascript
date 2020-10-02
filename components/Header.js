de910079d21f62504a13b854c5fa5b560b3be5d2
Andrew Poppenberg <andrewpoppenberg@Andrews-MacBook-Pro.local>
2020-10-02 10:24:58 -0500

    finished header

--- components/Header.js
+++ components/Header.js
@@ -11,21 +11,28 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {
-    const header = document.createElement('div')
-    const headerDate = document.createElement('span')
-    const headerH1 = document.createElement('h1')
-    const headerTemp = document.createElement('span')
+function header() {
+  const header = document.createElement("div");
+  const headerDate = document.createElement("span");
+  const headerH1 = document.createElement("h1");
+  const headerTemp = document.createElement("span");
+  const headerSpot = document.querySelector(".header-container");
 
-    header.classList.add('header')
-    headerDate.classList.add('date')
-    headerTemp.classList.add('temp')
+  header.classList.add('header');
+  headerDate.classList.add("date");
+  headerTemp.classList.add("temp");
 
-    header.appendChild(headerDate)
-    header.appendChild(headerH1)
-    header.appendChild(headerTemp)
+  headerDate.textContent = "March 28, 2020";
+  headerH1.textContent = "Lambda Times";
+  headerTemp.textContent = "98°";
 
-    const headerSpot = document.querySelector('header-container')
-    headerSpot.appendChild(header)
+  headerSpot.appendChild(header);
+  header.appendChild(headerDate);
+  header.appendChild(headerH1);
+  header.appendChild(headerTemp);
+
+  
+  return header
+  
 }
-header()
+header();
